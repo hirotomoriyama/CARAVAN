8775d7168c6c3194e2c68c6f376a0a038818fb86
hirotomoriyama <hiroto.moriyama7204@gmail.com>
2021-02-08 16:12:15 +0000

    [Finish]3-6:編集機能を作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,11 +15,16 @@ class BlogsController < ApplicationController
     blog = Blog.new(blog_params)
     blog.save
     redirect_to blogs_path(blog.id)
-
   end
 
   def edit
     @blog = Blog.find(params[:id])
+  end
+
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
   end
 
   private
